b372613ee6ceaed3e18ebe394ce2ee38788a196c
Sowmiya Sree Elavalagan <quic_ssreeela@quicinc.com>
2024-07-17 14:26:04 +0530

    wifi: ath12k: Add firmware coredump collection support
    In case of firmware assert snapshot of firmware memory is essential for
    debugging. Add firmware coredump collection support for PCI bus.
    Collect RDDM and firmware paging dumps from MHI and pack them in TLV
    format and also pack various memory shared during QMI phase in separate
    TLVs.  Add necessary header and share the dumps to user space using dev
    coredump framework. Coredump collection is disabled by default and can
    be enabled using menuconfig. Dump collected for a radio is 55 MB
    approximately.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.2.1-00201-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 WLAN.HMT.1.0-03427-QCAHMTSWPL_V1.0_V2.0_SILICONZ-1.15378.4
    
    Signed-off-by: Sowmiya Sree Elavalagan <quic_ssreeela@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Link: https://patch.msgid.link/20240717085604.4131642-1-quic_ssreeela@quicinc.com
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>

--- drivers/net/wireless/ath/ath12k/hw.c
+++ drivers/net/wireless/ath/ath12k/hw.c
@@ -946,7 +946,7 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.rfkill_cfg = 0,
 		.rfkill_on_level = 0,
 
-		.rddm_size = 0,
+		.rddm_size = 0x600000,
 
 		.def_num_link = 0,
 		.max_mlo_peer = 256,
@@ -1102,7 +1102,7 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.rfkill_cfg = 0,
 		.rfkill_on_level = 0,
 
-		.rddm_size = 0,
+		.rddm_size = 0x600000,
 
 		.def_num_link = 0,
 		.max_mlo_peer = 256,

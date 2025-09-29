f50c4e4b1132146b72fbf95b99c8e050fdca2781
GadiAvr <gadia@ceragon.com>
2025-09-29 10:43:13 +0300

    DEV-7525: 4addr configuration support in ath12k driver (#50)
    4addr functionality is currently not supported by the ath12k driver.
    This is core functionality required to pass traffic between Ethernet and
    wifi interfaces.
    I added code to force 4addr functionality.
    
    After changing the ath12k driver, only 4addr mode is working; the link
    can't be established without 4addr set to on.
    
    Establish a link with 4addr and verify traffic is passed between
    Ethernet and wifi interfaces.
    Checklist
    
    DEV-7525
    
    Co-authored-by: Gadi Avraham <gadia@vm-sw-gadia-workstation.ceragon.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -4850,8 +4850,11 @@ static int ath12k_mac_station_add(struct ath12k *ar,
 	ath12k_dbg(ab, ATH12K_DBG_MAC, "Added peer: %pM for VDEV: %d\n",
 		   sta->addr, arvif->vdev_id);
 
-	if (ieee80211_vif_is_mesh(vif)) {
-		ret = ath12k_wmi_set_peer_param(ar, sta->addr,
+	/* ------------------------------
+	   FORCE WDS / 4-ADDRESS MODE
+	   ----------------------------- */
+	// if (ieee80211_vif_is_mesh(vif)) {
+		ret = ath12k_wmi_set_peer_param(ar, arsta->addr,
 						arvif->vdev_id,
 						WMI_PEER_USE_4ADDR, 1);
 		if (ret) {
@@ -4859,7 +4862,7 @@ static int ath12k_mac_station_add(struct ath12k *ar,
 				    sta->addr, ret);
 			goto free_peer;
 		}
-	}
+	//}
 
 	ret = ath12k_dp_peer_setup(ar, arvif->vdev_id, sta->addr);
 	if (ret) {
